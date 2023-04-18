cab2c7861802d885bce2a263a997496108b3d7a1
Shreyansh-kankane <113857105+Shreyansh-kankane@users.noreply.github.com>
2023-04-18 20:04:26 +0530

    login auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import LoginButton from  "./components/login";
+import LogoutButton from "./components/logout";
+import { useAuth0 } from "@auth0/auth0-react";
+
 function App() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div>
+      {isAuthenticated ? 
+      <li> {user.name}  <LogoutButton></LogoutButton></li> 
+      : <li><LoginButton></LoginButton> </li> }
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-wclhnxebkh6zeijc.us.auth0.com"
+    clientId="ZJ38vJZqtHmEg7sPpWh9klgOa5ZplJPG"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>,
 );
